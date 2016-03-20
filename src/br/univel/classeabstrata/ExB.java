865f5e2e4434155cea41c4b025a7e2f218c5c049
Lucas Medeiros <lucas_medeiros09@hotmail.com>
2016-03-20 14:21:57 -0300

    Commit 3

---
+++ src/br/univel/classeabstrata/ExB.java
@@ -0,0 +1,27 @@
+package br.univel.classeabstrata;
+
+class ExB extends DesenhoAbstrato {
+
+	public static int ALTURA = 8;
+	
+	@Override
+	protected void desenhar() {
+				
+		int caracteres = ALTURA;
+		for (int linha = 0; linha < 8; linha++) {
+			for (int coluna = 0; coluna < caracteres; coluna++) {
+				System.out.print("#");
+			}
+			caracteres--;
+			System.out.print("\n");
+		}
+		
+	}
+
+	@Override
+	protected String getNome() {
+		// TODO Auto-generated method stub
+		return "B";
+	}
+
+}
