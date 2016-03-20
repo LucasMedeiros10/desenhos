ebbc209ce39d3eefeb9f956c9710be139600dd00
Lucas Medeiros <lucas_medeiros09@hotmail.com>
2016-03-20 14:26:43 -0300

    Commit 6

---
+++ src/br/univel/classeabstrata/ExE.java
@@ -0,0 +1,30 @@
+package br.univel.classeabstrata;
+
+class ExE extends DesenhoAbstrato {
+
+	public static int ALTURA = 8;
+
+	@Override
+	protected void desenhar() {
+
+		for (int linha = 0; linha < 8; linha++) {
+			for (int coluna = 0; coluna < ALTURA; coluna++) {
+
+				if (coluna == 0 || coluna == ALTURA - 1 || linha == 0 || linha == ALTURA - 1) {
+					System.out.print("#");
+				} else {
+					System.out.print(" ");
+				}
+			}
+			System.out.print("\n");
+		}
+
+	}
+
+	@Override
+	protected String getNome() {
+		// TODO Auto-generated method stub
+		return "E";
+	}
+
+}
